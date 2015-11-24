05831f36b027b29a410641dd61d3a58587dd2ad6
Paul Mather <pmather@vt.edu>
2015-11-24 18:23:07 -0500

    Fix install so "bootstrap.sh aws" works for ETDplus
    With the introduction of support for deployment from private Git
    repositories, the size of the install scripts and files/ material
    packed into the aws_bootstrapper.sh script exceeded the 16KB allowed
    for user-data.  (This was particularly true when a SSH deploy key was
    actually being used with the bootstrap.)  This caused the creation of
    AWS instances to fail.
    
    As a fix for this, the bootstrap_aws() function of bootstrap.sh was
    changed so that shell comments are removed from install*.sh and
    config{,_aws}.sh files before being packed into aws_bootstrapper.sh.
    The bootstrap_server.sh script is not stripped because it contains a
    "here document" that includes structured comments that are not safe to
    strip nor are straightforward to avoid stripping.  Shell comments are
    defined to be lines beginning with "#" (optionally preceded by
    whitespace), i.e., whole line comments, not comments appended to the
    end of shell code on a line.
    
    To make the comment removal more effective, the comments in the
    config{,_aws}.sh files were reformatted so they would be capable of
    being stripped.  This was also taken as an opportunity to expand the
    comments in those files to describe more of their contents.
    
    TMPDIR was changed in bootstrap_sh() because it has special meaning to
    mktemp and shouldn't be used in the context in which it was being used.
     It was changed to AWSDIR to avoid conflict.

--- config_aws.sh
+++ config_aws.sh
@@ -1,24 +1,38 @@
 #!/bin/sh
 # Defaults for AWS environment Installs
-INSTALL_USER="ubuntu" # Name of user to install under (must already exist)
-# Next three settings specify data.lib.vt.edu in production mode
-SERVER_HOSTNAME="data.lib.vt.edu" # The hostname of the server being installed.
-AWS_ELASTIC_IP="eipalloc-23c49346" # 54.152.247.163 - data.lib.vt.edu
-APP_ENV="production"
-# Next three settings specify datadev.lib.vt.edu in development mode
-#SERVER_HOSTNAME="datadev.lib.vt.edu" # The hostname of the server being installed.
-#AWS_ELASTIC_IP="eipalloc-4fc4932a" # 54.209.80.216 - datadev.lib.vt.edu
-#APP_ENV="development"
+# The git repository to pull changes from during setup.
+HYDRA_HEAD_GIT_REPO_URL="git@github.com:EducopiaInstitute/etdplus.git"
+# SSH deployment key, if any, needed for cloning above repository
+HYDRA_HEAD_GIT_REPO_DEPLOY_KEY="etdplus_deploykey"
+# The branch of the repository to pull.
+HYDRA_HEAD_GIT_BRANCH="master"
+# Name of user to install under (must already exist)
+INSTALL_USER="ubuntu"
+# The hostname of the server being installed.
+SERVER_HOSTNAME="52.5.42.55"
+# AWS Elastic IP pointing to $SERVER_HOSTNAME
+AWS_ELASTIC_IP="eipalloc-fe64959a"
+# RAILS_ENV to run under: "development" or "production"
+APP_ENV="development"
+# Name of Sufia Solr core
 SOLR_CORE="$APP_ENV"
 INSTALL_DIR="/home/$INSTALL_USER"
-FITS_DIR="$INSTALL_DIR/fits" # Where FITS will be installed.
-HYDRA_HEAD_DIR="$INSTALL_DIR/$HYDRA_HEAD" # Where the Hydra head will be located.
+# Where FITS will be installed.
+FITS_DIR="$INSTALL_DIR/fits"
+# Where the Hydra head will be located.
+HYDRA_HEAD_DIR="$INSTALL_DIR/$HYDRA_HEAD"
+# The directory in which the Fedora data lives
 FEDORA4_DATA="$INSTALL_DIR/fedora-data"
 RUN_AS_INSTALLUSER="sudo -H -u $INSTALL_USER"
+# AWS key pair used to connect to new instance
 AWS_KEY_PAIR="data_repo"
+# The AMI to be used for the newly-created AWS instance
 AWS_AMI="ami-d05e75b8"
+# The size in GB of the AWS instance EBS storage
 AWS_EBS_SIZE="16"
+# The type of AWS instance to use
 AWS_INSTANCE_TYPE="t2.medium"
+# The security groups to attach, e.g., to allow network access
 AWS_SECURITY_GROUP_IDS="sg-55159632 sg-e5149782"
+# AWS subnet in which to launch instance
 AWS_SUBNET_ID="subnet-ec703bb5"
-AWS_VPC_ID="vpc-bdd23fd9"
